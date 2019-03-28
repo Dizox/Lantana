0a294522e64fc48737d4a68953865a3a7bc49e63
Dizox <36742981+Dizox@users.noreply.github.com>
2019-03-28 12:21:17 +0300

    fixed pages build

--- dist/js/main.js
+++ dist/js/main.js
@@ -15302,6 +15302,15 @@ var _jquery = __webpack_require__(37);var _jquery2 = _interopRequireDefault(_jqu
 
 });
 
+(0, _jquery2.default)(document).scroll(function () {
+    var s_top = (0, _jquery2.default)(this).scrollTop();
+    if (s_top > 1) {
+        (0, _jquery2.default)(".header__logo").addClass("header__logo_sticky");
+    } else {
+        (0, _jquery2.default)(".header__logo").removeClass("header__logo_sticky");
+    }
+});
+
 /***/ })
 ],[38]);
 //# sourceMappingURL=main.js.map
